1355c85065fb1252396a399867ce4b2b27b53285
TineoC <christophertineo02@gmail.com>
2022-05-25 19:02:16 -0400

    Write test cases && scale out of range exception test

--- test/Temperature.Facts/UnitTest1.cs
+++ test/Temperature.Facts/UnitTest1.cs
@@ -13,18 +13,33 @@ namespace Temperature.Facts;
         ToCelsius() : ...
         ToKelvin() : ...
         ToString() : String // "5.4800F" 5.48C -5.48K
+
+        Test Cases:
+
+        - Fail Cases:
+
+        1. Input: Temperature(32, -1) Output: Throw ArgumentOutOfRangeException()
+
+        2. Input: Temperature(0.322, 3) Output: Throw Exception("Invalid Scale")
+
+        - Pass Cases:
+
+        1. Input: Temperature(10, 0) Output: "responds with Temperature class created".
+
+        2. Input: Temperature(50, 1) Output: "responds with Temperature class created"
+
+        3. Input: Temperature(-3, 2) Output: "responds with Temperature class created"
 */
+
+
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    // With Scale -1 Throws Argument
+    public void WithScaleMinusOneThrowsArgumentOutOfRangeException()
     {
-        Assert.Pass();
+        // With scale -1 throws Argum
+        // Assert.Pass(Temperature(32, -1));
     }
 }
